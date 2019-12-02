4e1e21eb463be6b7954f1f31aea47fbf212a9ea4
Simmers44 <simmersh@ripon.edu>
2019-12-02 19:33:33 +0000

    Complete overhaul of the choices. Changed to having functions and will now repeat questions if the correct input is not given

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 # name (temporary variable name) will be used for the player's name throughout the entire code
 name = input("Welcome to Adventure Game! Please input your name: ")
 print("Hello,", name, "let's begin. . .")
@@ -40,47 +42,69 @@ class Weapon():
 
 
 # Room 1 Example/Early stages. Each room will have its own function specific to it and will be called when approaching/entering each room
-print(name,", you awake in an unfamiliar room with seemingly random objects strewn about the small room around you. What do you do?")
-choice = input()
+def display_intro():
+  print(name,", you awake in an unfamiliar room with seemingly random objects strewn about the small room around you.")
 
 
 # this choice will determine what the player will be doing at that time. At this point very little is possible here with only a few options available. Note: all choices must be written in lower case in order for the action to be read and enacted correctly.
-if choice == "check surroundings":
-  print("There is a desk with many objects on it including an old desk top computer with a slightly open cabnet to its left. Many papers are scattered on the desk, possibly covering other unseen objects. Directly behind you is a closed and heavily secured locked door blocking your way out from behind. On both the left and right walls are singular doors, they seem to be unlocked.")
-  rm1_choice = input("What do you do?: ")
-else:
-  print("That action could not be done. Please try again")
-  choice = input()
+def intro_choice():
+  correct_choice = "check surroundings"
+  while True:
+    introchoice = input("What do you do?: ")
+    if introchoice == correct_choice:
+      print("There is a desk with many objects on it including an old desk top computer with a slightly open cabnet to its left.")
+      time.sleep(2)
+      print("Many papers are scattered on the desk, possibly covering other unseen objects.")
+      time.sleep(2)
+      print("Directly behind you is a closed and heavily secured locked door blocking your way out from behind.")
+      time.sleep(2)
+      print("On both the left and right walls are singular doors, they seem to be unlocked.")
+      break
+    else:
+      print("That action could not be done. Please try again.")
 
 # the first room choice will allow the player to check certain things in their surroundings such as the desk and the three seperate doors in the room. By checking the desk, the player is given the opportunity to find their first weapon that can be either picked up or left at the desk. That decision regarding the knife will have consequences ;ater. Two of the doors lead to new rooms, thus creating the first branching path of the game.
-if rm1_choice == "check desk":
-  print("The desk is cluttered with mostly crumpled up pieces of paper and notes filled with what seems like nonsense. After shuffling through the mess, you find a rusty pocket knife. It is difficult to move but it is still sharp and may become useful")
-  
-  knife_choice = input("Will you take the knife?: ")
-elif rm1_choice == "check the door behind me":
-  print("The door is very heavily secured and is unable to open. You are not getting out this way. Try something else")
-  
-  rm1_choice = input("What do you do?: ")
-elif rm1_choice == "check the right door":
-  print("You approach the door on the right wall. The door is considerable large and may take some force to move it. You push hard on it and it opens easier than you thought. Your momentum makes you stumble and fall into another unfamiliar room.")
-elif rm1_choice == "check the left door":
-  print("You approach the door on the left wall. It looks worse for wear and could almost be broken if pushed too hard. You delicately puch on the door but nothing happens. You slowly increse pressure on the door but still nothing. You grow impatient and put your shoulder into your efforts and bust through the door. When looking back the door never actually opened, rather you broke right through the delapidated boards keeping it together. You made it through, an exciting and dangerous way but you made it. The door lead to another strange room.")
-else:
-  print("That action could not be done. Please try again")
- 
-  rm1_choice = input("What do you do?: ")
+def room1_choice1():
+  correct1_choice1 = "check desk"
+  correct1_choice2 = "check back door"
+  correct1_choice3 = "check right door"
+  correct1_choice4 = "check left door"
+  while True:
+    choice1 = input("Now, what do you do?: ")
+    if choice1 == correct1_choice1:
+      print("The desk is cluttered with mostly crumpled up pieces of paper and notes filled with what seems like nonsense.")
+      time.sleep(2)
+      print("After shuffling through the mess, you find a rusty pocket knife.")
+      time.sleep(2)
+      print("It is difficult to move but it is still sharp and may become useful")
+      break
+    elif choice1 == correct1_choice2:
+      print("The door is very heavily secured and is unable to open.")
+      time.sleep(2)
+      print("You are not getting out this way. Try something else")
+    elif choice1 == correct1_choice3:
+      print("You approach the door on the right wall. The door is considerable large and may take some force to move it.")
+      time.sleep(2)
+      print("You push hard on it and it opens easier than you thought.")
+      time.sleep(2)
+      print("Your momentum makes you stumble and fall into another unfamiliar room.")
+      break
+    elif choice1 == correct1_choice4:
+      print("You approach the door on the left wall. It looks worse for wear and could almost be broken if pushed too hard.")
+      time.sleep(2)
+      print("You delicately push on the door but nothing happens.")
+      time.sleep(2)
+      print("You slowly increse pressure on the door but still nothing.")
+      time.sleep(2)
+      print("You grow impatient and put your shoulder into your efforts and bust through the door.")
+      time.sleep(2)
+      print("When looking back the door never actually opened, rather you broke right through the delapidated boards keeping it together.")
+      time.sleep(2)
+      print("You made it through, an exciting and dangerous way but you made it and as it seems, the door lead to another strange room.")
+      break
+    else:
+      print("That action could not be done. Please try again.")
 
-# the knife choice scenarios determine whether or not the player takes the knife as a weapon or tool for later in the game. The knife will be directly added to the inventory and assigned as the current player weapon if taken. If left on the desk, it will not be added to the inventory and may have lasting consequences in later rooms and encounters.
-if knife_choice == "yes":
-  print("You stuggle to put away the blade of the knife but after some time you get it become loose again. The knife is useful now and may become useful in later situations. You place it in your back pocket and move on.")
-  
-  # player.append(Player(name, 100, Weapon("knife", 20)))
-  # player.add_item("knife")
-  rm1_choice = input("Now with the knife, what do you do?: ")
-elif knife_choice == "no":
-  print("You ponder the thought of taking the knife but ultimately choose to not take it. You place it back down on the desk, turning away. Out of sight, out of mind.")
-  
-  rm1_choice = input("Now without the knife, what do you do?: ")
-else:
-  print("That action could not be done. Please try again")
-  knife_choice = ("Will you take the knife?: ")
+display_intro()
+intro_choice()
+room1_choice1()
